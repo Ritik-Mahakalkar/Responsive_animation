2698e104331840ce68d763301d8f2a4c3cbb2c37
Ritik Mahakalkar <153834897+Ritik-Mahakalkar@users.noreply.github.com>
2025-03-21 14:56:07 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,21 @@
+ document.addEventListener("DOMContentLoaded", () => {
+            setTimeout(() => {
+                document.querySelector(".loader").style.display = "none";
+                document.querySelector(".content").style.display = "block";
+                document.querySelector(".video-background").style.display = "block"; // Show video after loader
+                animateText("Mahakalkar");
+            }, 4000);
+        });
+
+        function animateText(text) {
+            let i = 0;
+            const target = document.getElementById("animated-text");
+            function type() {
+                if (i < text.length) {
+                    target.textContent += text[i];
+                    i++;
+                    setTimeout(type, 400);
+                }
+            }
+            type();
+        }
